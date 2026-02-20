80218e9c4f071579469a1ab1c1576b47bfd95150
ProgrammerKR <programmerkr.123@gmail.com>
2026-02-20 13:50:18 +0530

    fix all errors.

--- build/lib/builtInExtensions.ts
+++ build/lib/builtInExtensions.ts
@@ -1,7 +1,8 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Copyright (c) ProXentix. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
 
 import fs from 'fs';
 import path from 'path';
