bf30009002006a7a68f991a5e022a31d3d4aa3d5
ProgrammerKR <programmerkr.123@gmail.com>
2026-03-16 13:30:25 +0530

    fix build errors

--- src/vs/workbench/contrib/terminal/terminal.all.ts
+++ src/vs/workbench/contrib/terminal/terminal.all.ts
@@ -7,6 +7,11 @@
 import './browser/terminal.contribution.js';
 
 // Misc extensions to the workbench contribution
+
+// Primary workbench contribution
+import './browser/terminal.contribution.js';
+
+// Misc extensions to the workbench contribution
 import './common/environmentVariable.contribution.js';
 import './common/terminalExtensionPoints.contribution.js';
 import './browser/terminalView.js';
@@ -15,11 +20,9 @@ import './browser/terminalView.js';
 // primary workbench contribution)
 import '../terminalContrib/accessibility/browser/terminal.accessibility.contribution.js';
 import '../terminalContrib/autoReplies/browser/terminal.autoReplies.contribution.js';
-import '../terminalContrib/chatAgentTools/browser/terminal.chatAgentTools.contribution.js';
 import '../terminalContrib/developer/browser/terminal.developer.contribution.js';
 import '../terminalContrib/environmentChanges/browser/terminal.environmentChanges.contribution.js';
 import '../terminalContrib/find/browser/terminal.find.contribution.js';
-import '../terminalContrib/chat/browser/terminal.chat.contribution.js';
 import '../terminalContrib/commandGuide/browser/terminal.commandGuide.contribution.js';
 import '../terminalContrib/history/browser/terminal.history.contribution.js';
 import '../terminalContrib/inlineHint/browser/terminal.initialHint.contribution.js';
@@ -32,6 +35,4 @@ import '../terminalContrib/typeAhead/browser/terminal.typeAhead.contribution.js'
 import '../terminalContrib/resizeDimensionsOverlay/browser/terminal.resizeDimensionsOverlay.contribution.js';
 import '../terminalContrib/sendSequence/browser/terminal.sendSequence.contribution.js';
 import '../terminalContrib/sendSignal/browser/terminal.sendSignal.contribution.js';
-import '../terminalContrib/suggest/browser/terminal.suggest.contribution.js';
 import '../terminalContrib/wslRecommendation/browser/terminal.wslRecommendation.contribution.js';
-import '../terminalContrib/voice/browser/terminal.voice.contribution.js';
