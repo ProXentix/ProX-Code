b2d066e170ce0a78b182a2161cee2407fbe528bc
ProgrammerKR <programmerkr.123@gmail.com>
2026-08-02 23:35:31 +0530

    fix: update refreshSearch method call in searchActionsTopBar

--- src/vs/workbench/contrib/search/browser/searchActionsTopBar.ts
+++ src/vs/workbench/contrib/search/browser/searchActionsTopBar.ts
@@ -266,7 +266,7 @@ function cancelSearch(accessor: ServicesAccessor) {
 function refreshSearch(accessor: ServicesAccessor) {
 	const viewsService = accessor.get(IViewsService);
 	const searchView = getSearchView(viewsService);
-	searchView?.triggerSearch();
+	searchView?.searchWidget.focus();
 }
 
 function collapseDeepestExpandedLevel(accessor: ServicesAccessor) {
